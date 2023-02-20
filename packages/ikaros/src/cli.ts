c37380073a02be8fb3f2adc9ad95f2e4b2e3519d
umbrella22 <1035765115@qq.com>
2023-02-20 18:52:14 +0800

    chore: init project

---
+++ packages/ikaros/src/cli.ts
@@ -0,0 +1,35 @@
+import { cac } from 'cac'
+import type { LogLevel } from './node/logger'
+import { createLogger } from './node/logger'
+import { VERSION } from './node/constants'
+
+const cli = cac('ikaros')
+
+cli
+  .option('-m , --mode <mode>', '[dev | prod | test | sit | ...] 环境模式 ')
+  .option('dev, server', '开发环境启动,允许衔接m[mode]参数来控制当前环境')
+  .option('build', '打包模式，')
+
+interface GlobalCLIOptions {
+  '--': string[]
+  m: string
+  mode: string
+}
+cli
+  .command('[config-file]', 'start dev server')
+  .alias('dev')
+  .alias('server')
+  .action(async (configFile: undefined | string, options: GlobalCLIOptions) => {
+    console.log('dev mode', options)
+  })
+
+cli
+  .command('build [root]', 'build app with mode')
+  .alias('build')
+  .action(async (configFile: undefined | string, options: GlobalCLIOptions) => {
+    console.log('build mode', options)
+  })
+
+cli.help()
+cli.version(VERSION)
+cli.parse()
